970244ca134113b32d8fb0aee7d38a7d313f01a1
Peter Harry <Peter.harry56@gmail.com>
2018-08-03 17:51:05 +1000

    Update install_14.04.sh
    Updated to oracle-java8-installer as java6 no longer available

--- scripts/install_14.04.sh
+++ scripts/install_14.04.sh
@@ -6,4 +6,4 @@ apt-get install -y git-core gnupg flex bison gperf build-essential zip curl \
 	zlib1g-dev libc6-dev lib32ncurses5-dev lib32z1 lib32ncurses5 lib32bz2-1.0 \
 	x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev g++-multilib mingw32 \
 	tofrodos python-markdown libxml2-utils libglapi-mesa:i386 u-boot-tools \
-	oracle-java6-installer libswitch-perl xsltproc
+	oracle-java8-installer libswitch-perl xsltproc
