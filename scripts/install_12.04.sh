0a691721eaa3decd7c5379a2ad80bdd5565827b9
Peter Harry <Peter.harry56@gmail.com>
2018-08-03 17:50:06 +1000

    Update install_12.04.sh
    Updated to oracle-java8-installer

--- scripts/install_12.04.sh
+++ scripts/install_12.04.sh
@@ -6,4 +6,4 @@ apt-get install -y git-core gnupg flex bison gperf build-essential zip curl \
 	zlib1g-dev libc6-dev lib32ncurses5-dev python-markdown \
 	ia32-libs x11proto-core-dev libx11-dev \
 	lib32z1-dev libgl1-mesa-dev g++-multilib mingw32 tofrodos \
-	libxml2-utils libglapi-mesa:i386 uboot-mkimage oracle-java6-installer xsltproc
+	libxml2-utils libglapi-mesa:i386 uboot-mkimage oracle-java8-installer xsltproc
