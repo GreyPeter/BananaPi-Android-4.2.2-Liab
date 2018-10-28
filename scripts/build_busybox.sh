046c304732753c144caffcc1d9406477a32fea08
Peter Harry <Peter.harry56@gmail.com>
2018-10-28 12:10:25 +1000

    Update build_busybox.sh
    Select correct cross compiler for this build. Will need to update for MacOS

--- scripts/build_busybox.sh
+++ scripts/build_busybox.sh
@@ -4,6 +4,6 @@ export LANG=C
 
 cd ../busybox-1.23.1
 export PATH=$PATH:../lichee/out/android/common/buildroot/external-toolchain/bin/
-make
+make CROSS_COMPILE=arm-linux-gnueabi-
 cp busybox ../android/device/softwinner/common/bin
 cp busybox ../android/device/softwinner/wing-common
